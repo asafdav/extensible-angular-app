c8a4495078a1ce6f6c620857aa21a8339f96744f
Asaf David <asafdav@gmail.com>
2015-05-14 21:51:19 +0300

    Rev images in .js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -279,8 +279,9 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/**/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      js: ['<%= yeoman.dist %>/scripts/**/*.js'],
       options: {
         assetsDirs: [
           '<%= yeoman.dist %>',
